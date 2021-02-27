79c9e824fb7d4eaf54ec00eac2aef838e7da3387
jiweon <kor98won@gmail.com>
2021-02-28 00:53:31 +0900

    print, scan

---
+++ week01/GetTheInput.java
@@ -0,0 +1,16 @@
+package week01;
+import java.util.Scanner;
+
+public class GetTheInput {
+
+	public static void main(String[] args) {
+		// create Scanner to obtain input from command line
+		Scanner input = new Scanner(System.in);
+		int i = input.nextInt();
+		String str = input.nextLine();
+		input.close();
+		System.out.println(i);
+		System.out.println(str);
+	}
+
+}
